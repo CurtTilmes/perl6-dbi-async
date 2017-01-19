a6b403971f8d59e78056112202402e4ef2c43549
Curt Tilmes <Curt.Tilmes@nasa.gov>
2017-01-19 17:04:14 -0500

    Change example to 100 queries

--- eg/parallel.pl
+++ eg/parallel.pl
@@ -2,17 +2,17 @@
 
 use DBI::Async;
 
-my $db = DBI::Async.new('Pg', connections => 2);
+my $db = DBI::Async.new('Pg', connections => 5);
 
 my @list;
 
-for 1..10
+for 1..100
 {
     @list.push(start {
         say "starting $_";
-        my $res = $db.query("select pg_sleep(1)::text, ?::int as val", $_);
-        say "Done #", $res.row[1];
-        $res.finish;
+
+        say "Done #", $db.query("select pg_sleep(1)::text, ?::int as val",
+                                $_).array[1];
     });
 }
 
