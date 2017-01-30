8037962ee11d3acb1419eab30597fc75f2fb2100
Curt Tilmes <Curt.Tilmes@nasa.gov>
2017-01-30 11:04:47 -0500

    Add another promise example

--- eg/promise.pl
+++ eg/promise.pl
@@ -8,6 +8,10 @@ $db.query("select version() || pg_sleep(1)", :async).then(-> $p {
     say $p.result.array[0];
 });
 
+my $p = $db.query("select version() || pg_sleep(1)", :async);
+
+say $p.result.array[0];
+
 sleep 2;
 
 say "done";
